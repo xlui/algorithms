9e515fc70f0659a98c535481511673c737f9daf7
xlui <liuqi0315@gmail.com>
2018-03-07 14:12:23 +0800

    Problem 717

--- Code/src/me/xlui/algo/Main.java
+++ Code/src/me/xlui/algo/Main.java
@@ -1,15 +1,12 @@
 package me.xlui.algo;
 
-import me.xlui.algo.Problem748.Solution;
+import me.xlui.algo.Problem717.Solution;
 
 public class Main {
 	public static void main(String[] args) {
-//		String licensePlate = "1s3 PSt";
-//		String[] words = new String[]{"step", "steps", "stripe", "stepple"};
-//		String licensePlate = "1s3 456";
-//		String[] words = new String[]{"looks", "pest", "stew", "show"};
-		String licensePlate = "Ah71752";
-		String[] words = new String[]{"suggest", "letter", "of", "husband", "easy", "education", "drug", "prevent", "writer", "old"};
-		System.out.println(Solution.shortestCompletingWord(licensePlate, words));
+//		int[] bits = new int[]{1, 1, 0};
+//		int[] bits = new int[]{1, 1, 1, 0};
+		int[] bits = new int[]{1, 1, 1, 0, 0, 0, 1, 0};
+		System.out.println(Solution.isOneBitCharacter(bits));
 	}
 }
---
+++ Code/src/me/xlui/algo/Problem717/Solution.java
@@ -0,0 +1,22 @@
+package me.xlui.algo.Problem717;
+
+/**
+ * 110 true
+ * 1110 false
+ * 111010 false
+ *
+ * 算法的思想是通过索引 i 的跳转，遇到 1 跳两步，遇到 0 跳一步。
+ * 当 while 循环结束的时候，通过 i 的位置来判断，如果 i 的位置刚好是 bits 数组最后一个元素，则 true；如果 i 的位置跳过了最后一个元素，则 false
+ */
+public class Solution {
+	public static boolean isOneBitCharacter(int[] bits) {
+		int len = bits.length, i = 0;
+		while (i < len - 1) {
+			if (0 == bits[i])
+				i++;
+			else
+				i += 2;
+		}
+		return i == len - 1;
+	}
+}
